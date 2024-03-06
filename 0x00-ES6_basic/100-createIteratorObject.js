b697c3b2c46cc96c14dff9e03c56ab2883195f7f
cholthi <cholthipaul@gmail.com>
2024-03-06 16:20:47 +0200

    added 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const allEmployees = [];
   for (const dep in report.allEmployees) {
-    if (report.allEmployees.hasOwnProperty(dep)) {
+    if (dep) {
       allEmployees.push(...report.allEmployees[dep]);
     }
   }
