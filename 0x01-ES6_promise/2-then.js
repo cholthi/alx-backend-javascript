c2ba1f9abdcab4ff0492401d0d9370c5ce2630b2
cholthi <cholthipaul@gmail.com>
2024-03-08 09:22:59 +0200

    correction for 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'Success',
-  }))
-    .catch(() => new Error())
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
+export default handleResponseFromAPI;
